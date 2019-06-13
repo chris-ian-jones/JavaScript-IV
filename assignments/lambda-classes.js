1e11f22896424ff3dfc1958b5151a96e0d91cc3c
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-13 22:11:32 +0100

    Completed: lambda-classes.js tasks

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,11 +3,11 @@ class Person {
         this.name = props.name;
         this.age = props.age;
         this.location = props.location;
-    }
+    };
     speak() {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
-    }
-}
+    };
+};
 
 class Instructor extends Person {
     constructor(props) {
@@ -15,14 +15,14 @@ class Instructor extends Person {
         this.speciality = props.speciality;
         this.favLanguage = props.favLanguage;
         this.catchPhrase = props.catchPhrase;
-    }
+    };
     demo(subject) {
         console.log(`Today we are learning about ${subject}`);
-    }
+    };
     grade(student, subject) {
         console.log(`${student.name} receives a perfect score on ${subject}`);
-    }
-}
+    };
+};
 
 class Student extends Person {
     constructor(props) {
@@ -30,69 +30,160 @@ class Student extends Person {
         this.previousBackground = props.previousBackground;
         this.className = props.className;
         this.favSubjects = props.favSubjects;
-    }
+    };
     listsSubjects() {
         console.log(`${this.favSubjects.join(' ')}`);
-    }
+    };
     PRAssignment(subject) {
         console.log(`${this.name} has submitted a PR for ${subject}`);
-    }
+    };
     sprintChallenge(subject) {
         console.log(`${this.name} has begun sprint challenge on ${subject}`);
-    }
-}
+    };
+};
 
 class ProjectManager extends Instructor {
     constructor(props) {
         super(props);
         this.gradClassName = props.gradClassName;
         this.favInstructor = props.favInstructor;
-    }
+    };
     standup(slackChannel) {
-        console.log(`${this.name} announces to ${slackChannel}, @channel standy times!​​​​​`)
-    }
+        console.log(`${this.name} announces to ${slackChannel}, @channel standy times!​​​​​`);
+    };
     debugsCode(student, subject) {
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
-    }
-}
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    };
+};
 
-//Test Student
-const chris = new Student({
-    name: 'Chris',
-    location: 'Oxford',
-    age: 33,
-    previousBackground: 'PREVIOUSBACKGROUND',
-    className: 'CLASSNAME',
-    favSubjects: ['FAVSUBJECTS1', 'FAVSUBJECT2', 'FAVSUBJECTS']
-})
+// Students
 
-//Test Instructor
-const fred = new Instructor({
-    name: 'Fred',
-    location: 'Bedrock',
-    age: 37,
-    specialty: 'SPECIALITY',
-    favLanguage: 'FAVLANGUAGE',
-    catchPhrase: 'CATCHPHRASE'
+const isaiah = new Student({
+    name: 'Isaiah',
+    age: 18,
+    location: 'Florida',
+    previousBackground: 'High School last month',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript']
 });
 
-//Test ProjectManager
+const kevin = new Student({
+    name: "Kevin",
+    age: 28,
+    location: "California",
+    previousBackground: "Table Games Dealer",
+    className: "WEB21",
+    favSubjects: ['Html', 'JavaScript']
+});
+
+const theo = new Student({
+    name: "Theo",
+    age: 35,
+    location: "Louisiana",
+    previousBackground: "Comedian",
+    className: "WEB21",
+    favSubjects: ['Python', 'Go', 'JavaScript']
+});
+
+// Instructors
+
+const dan = new Instructor({
+    name: 'Dan',
+    age: 'Infinity',
+    location: 'Denver',
+    specialty: 'Relentless Debugger',
+    favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia',
+    catchPhrase: 'If you can do the thing, you can get paid to do the thing!'
+});
+
+const nisa = new Instructor({
+    name: 'Nisa',
+    age: 25,
+    location: 'Ohio',
+    specialty: 'Relentless Debugger',
+    favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia',
+    catchPhrase: 'If you can do the thing, you can get paid to do the thing!'
+});
+
+const joe = new Instructor({
+    name: 'Joe',
+    age: '50',
+    location: 'Los Angeles',
+    specialty: 'Youtube Tutorials',
+    favLanguage: 'Java, Ruby, and Julia',
+    catchPhrase: 'You got this!'
+});
+
+// Project Managers
+
 const mary = new ProjectManager({
     name: 'Mary',
-    location: 'PMLOCATION',
-    age: 37,
-    specialty: 'SPECIALITY',
-    favLanguage: 'FAVLANGUAGE',
-    catchPhrase: 'CATCHPHRASE',
-    gradClassName: 'GRADCLASSNAME',
-    favInstructor: 'FAVINSTRUCTOR'
-})
+    age: '24',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'New York',
+    specialty: 'Express and Node.js',
+    favLanguage: 'Javascript',
+    catchPhrase: "That looks AWESOME"
+});
+
+const austin = new ProjectManager({
+    name: 'Austin',
+    age: '23',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh knell',
+    location: 'Somewhere',
+    specialty: 'Java',
+    favLanguage: 'Java',
+    catchPhrase: ":eggplant:"
+});
+
+const marguel = new ProjectManager({
+    name: 'Marguel',
+    age: 'Maybe 26',
+    gradClassName: 'WEBPT2',
+    favInstructor: 'Me?',
+    location: 'California',
+    specialty: 'React',
+    favLanguage: 'JavaScript, Python, Elm etc.',
+    catchPhrase: "Practice Flex Zombies !!!"
+});
+
+// * Method Tests * //
+
+// Person
+isaiah.speak();
+dan.speak();
+mary.speak();
+
+// Instructor
+dan.demo('User-Interface');
+nisa.demo('UI-III-Flexbox');
+joe.demo('Git-Flow-Practice');
+
+dan.grade(isaiah, 'JavaScript-I');
+nisa.grade(kevin, 'Sprint-Challenge--Advanced-CSS');
+joe.grade(theo, 'Preprocessing-II');
+
+// Student
+isaiah.listsSubjects();
+kevin.listsSubjects();
+theo.listsSubjects();
+
+isaiah.PRAssignment('Preprocessing-I');
+kevin.PRAssignment('portfolio-website');
+theo.PRAssignment('responsive-web-design-I');
 
 
-fred.demo('ja');
-fred.grade(chris, 'SUBJECT STRING');
-chris.listsSubjects();
-chris.PRAssignment('J-IV');
-chris.sprintChallenge('J-VII');
-mary.standup('#help');
-mary.debugsCode(chris, 'J-XI');
+isaiah.sprintChallenge('Sprint-Challenge--User-Interface');
+kevin.sprintChallenge('Sprint-Challenge--Advanced-CSS');
+theo.sprintChallenge('Sprint-Challenge--Javascript');
+
+// Project Manager
+mary.standup('#Web21Mary');
+austin.standup('#Web21Austin');
+marguel.standup('#Web21Marguel');
+
+mary.debugsCode(isaiah, 'JavaScript-I');
+austin.debugsCode(kevin, 'Sprint-Challenge--Advanced-CSS');
+marguel.debugsCode(theo, 'Preprocessing-II');
