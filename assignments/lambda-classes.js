d4401e9f1ca1279ed760c27a55eef8c080890c31
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-13 20:27:26 +0100

    WIP: lambda-classes - created 'Person' base-class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,19 @@
-// CODE here for your Lambda Classes
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+// Test Person
+const fred = new Person({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+});
+
+fred.speak();
